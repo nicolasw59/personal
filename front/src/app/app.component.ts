097c0cd53d22a957c60a6014f170b3a424f56a2e
Nicolas Wychowski <nicolas.wychowski@eviden.com>
2025-03-19 23:54:56 +0100

    feat: affichage des informations produits

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -5,13 +5,14 @@ import { RouterModule } from "@angular/router";
 import { SplitterModule } from 'primeng/splitter';
 import { ToolbarModule } from 'primeng/toolbar';
 import { PanelMenuComponent } from "./shared/ui/panel-menu/panel-menu.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
   standalone: true,
-  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent],
+  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent, CommonModule],
 })
 export class AppComponent {
   title = "ALTEN SHOP";
